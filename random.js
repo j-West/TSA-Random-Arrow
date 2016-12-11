5766c29a7f282af9694ebf722c497242a92a92bd
j-west <westja89@gmail.com>
2016-12-11 12:00:49 -0600

    Add if statement to check what the value of decision is, 0 or 1.  If 0 the imgEl.src is updated with the appropriate data, then the hidden-class is toggled on the imgEl.  A timeout is set for one second, that returns the imgEl src to #, and toggles the hidden-class on the imgEl, also the addListener function is called which add's the event listener back onto the htmlEl

--- random.js
+++ random.js
@@ -7,8 +7,19 @@ function addListener() {
 
 function randomNum() {
   var decision = Math.round(Math.random());
-
   htmlEl.removeEventListener("click", randomNum)
+
+  if (decision === 0) {
+    imgEl.src = "images/rightArrow.png"
+    imgEl.classList.toggle("hidden-class")
+    console.log(decision);
+      setTimeout(function(){
+        imgEl.src = "#"
+        imgEl.classList.toggle("hidden-class")
+        addListener()
+      }, 1000);
+  }
+
 }
 
 addListener()
