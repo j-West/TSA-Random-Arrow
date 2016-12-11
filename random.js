16313e8fe30205fbd84e146f03649c3c031303cd
j-west <westja89@gmail.com>
2016-12-11 12:02:00 -0600

    Else statement which does the same as the if statement, only using a different image for the imgEl source

--- random.js
+++ random.js
@@ -18,7 +18,16 @@ function randomNum() {
         imgEl.classList.toggle("hidden-class")
         addListener()
       }, 1000);
-  }
+  } else {
+      imgEl.src = "images/leftArrow.png"
+      imgEl.classList.toggle("hidden-class")
+      console.log(decision);
+        setTimeout(function(){
+          imgEl.src = "#"
+          imgEl.classList.toggle("hidden-class")
+          addListener()
+        }, 1000);
+    }
 
 }
 
